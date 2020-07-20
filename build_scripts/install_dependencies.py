e466b0ab286e1f06ad94fc999e10eef4280ba41e
Brijesk <brijeshk_raj@yahoo.com>
2020-07-20 17:29:23 +0530

    sample

--- build_scripts/install_dependencies.py
+++ build_scripts/install_dependencies.py
@@ -10,3 +10,7 @@ def install_pip():
     pip_install_txt = os.path.join(os.path.abspath(os.path.join(__file__, os.pardir)), "build_test_dependencies.txt")
     call_subprocess("python3 -m pip install -r %s" % pip_install_txt)
     print("Stage install dependencies -- COMPLETED --")
+
+
+if __name__ == '__main__':
+    install_pip()
